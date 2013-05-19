7b75ad7f1bb0e5078ae350fb6f11f2d84909c263
calamari <g.tavonius@gmail.com>
2013-05-19 17:14:10 +0200

    add secondary column and some css

--- spec/views/articles/index.html.erb_spec.rb
+++ spec/views/articles/index.html.erb_spec.rb
@@ -3,27 +3,21 @@ require 'spec_helper'
 describe "articles/index" do
   before(:each) do
     assign(:articles, [
-      stub_model(Article,
+      Fabricate(:published_article,
         :title => "Title",
-        :text => "MyText",
-        :html => "MyText2",
-        :slug => "Slug"
+        :text => "MyText"
       ),
-      stub_model(Article,
+      Fabricate(:published_article,
         :title => "Title",
-        :text => "MyText",
-        :html => "MyText2",
-        :slug => "Slug"
+        :text => "MyText"
       )
     ])
   end
 
   it "renders a list of articles" do
     render
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "Title".to_s, :count => 2
-    assert_select "tr>td", :text => "MyText".to_s, :count => 2
-    assert_select "tr>td", :text => "MyText2".to_s, :count => 2
-    assert_select "tr>td", :text => "Slug".to_s, :count => 2
+
+    expect(page).to have_content("Title", count: 2)
+    expect(page).to have_content("MyText", count: 2)
   end
 end
