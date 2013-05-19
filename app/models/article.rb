ee94f1fb1ceb9e24c54fe360f68cd7060b3095f1
calamari <g.tavonius@gmail.com>
2013-05-19 14:07:29 +0200

    draft and published scopes for articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,6 +3,9 @@ class Article < ActiveRecord::Base
 
   validates_presence_of :html, :slug, :text, :title
 
+  scope :draft, lambda { where(:published_at => nil) }
+  scope :published, lambda { where('published_at IS NOT NULL') }
+
   def draft?
     published_at.nil?
   end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -12,4 +12,47 @@ describe Article do
     it { subject.published?.should_not be true }
     it { subject.draft?.should be true }
   end
+
+
+  describe "draft scope" do
+    describe "when having no drafts" do
+      let!(:article1) { Fabricate(:article, :published_at => Time.now) }
+      let!(:article2) { Fabricate(:article, :published_at => Time.now) }
+
+      subject { Article.draft }
+
+      it { should be_empty }
+    end
+
+    describe "when having draft articles" do
+      let!(:article1) { Fabricate(:article) }
+      let!(:article2) { Fabricate(:article, :published_at => Time.now) }
+
+      subject { Article.draft }
+
+      it { subject.length.should be 1 }
+      it { subject.should include(article1) }
+    end
+  end
+
+  describe "published scope" do
+    describe "when having no published articles" do
+      let!(:article1) { Fabricate(:article) }
+      let!(:article2) { Fabricate(:article) }
+
+      subject { Article.published }
+
+      it { should be_empty }
+    end
+
+    describe "when having published articles" do
+      let!(:article1) { Fabricate(:article) }
+      let!(:article2) { Fabricate(:article, :published_at => Time.now) }
+
+      subject { Article.published.all }
+
+      it { subject.length.should be 1 }
+      it { subject.should include(article2) }
+    end
+  end
 end
