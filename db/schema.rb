f4ae2804839c2b87231fe4a021091683c6151802
calamari <g.tavonius@gmail.com>
2013-06-02 08:58:15 +0200

    added homepage to user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130601121219) do
+ActiveRecord::Schema.define(:version => 20130602065346) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(:version => 20130601121219) do
     t.string   "password_digest"
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
+    t.string   "homepage"
   end
 
 end
