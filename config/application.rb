211e67b0523a7ecae5bdf2e8e85a9d575b3d6fd7
Calamari <g.tavonius@gmail.com>
2014-01-05 12:27:30 +0100

    Removing deprecation warning

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module Penpaper
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Skip deprecation warning
+    I18n.enforce_available_locales = false
   end
 end
